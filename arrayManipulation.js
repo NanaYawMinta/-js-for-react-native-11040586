a8ae05a2ff04f2bc88d46fea67b9a62151246b13
NanaYawMinta <okatakyieminta8@gmail.com>
2024-05-19 20:19:00 +0000

    completed task 2: add a function formatArrayStrings

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -15,3 +15,20 @@ function processArray(numbers){
 const numbers = [1,2,3,4,5,6,7,8,9];
 const result = processArray(numbers);
 console.log(result);
+
+
+function formatArrayString(strings,numbers){
+    return strings.map((str,index) => {
+        if (numbers[index] %  2 ===0) {
+            return str.toUpperCase();
+        }
+        else {
+            return str.toLowerCase();
+        }
+        
+    });
+}
+
+const strings = ["Hello", "JavaScript", "you", "are", "welcome"];
+const formattedStrings = formatArrayString(strings, result);
+console.log(formattedStrings);
